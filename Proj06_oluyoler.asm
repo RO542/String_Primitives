64c72cc2844997ab1bbe4568420b932f8e749e8b
oluyoler <oluyoler@oregonstate.edu>
2023-08-15 20:55:51 -0400

    working conversion of string

--- Proj06_oluyoler.asm
+++ Proj06_oluyoler.asm
@@ -58,12 +58,15 @@ byteCount	DWORD   ?
 average		SDWORD ?
 sum			SDWORD ?
 
+tempVar		SDWORD ?
+tempVar2	SDWORD ?
+
 intro1		BYTE "The following program takes in 10 user inputs as strings verifies they are valid and returns the rounded average and sum that is represented.",0
 intro2		BYTE "The inputs must represent numbers [-2^31,2^31-1] to be valid ",0
 intro3		BYTE "After collecting 10 numbers the running sum and rounded average will be displayed.",0
 
 promptUser	BYTE "Enter in a signed number: ",0
-rePrompt	BYTE "Re-enter a number,make sure it is in the stated bounds and contains only digits without other symbols",0
+rePrompt	BYTE "What you entered is either too large,too small, or not a number.Try again: ",0
 
 YouEntered	BYTE "Here are your 10 inputs",0
 showAverage	BYTE "The truncated average is:",0
@@ -72,6 +75,7 @@ goodbye		BYTE "Thanks for using the program,bye now.",0
 
 invalid		byte "invalid",0
 valid		byte "valid",0
+testNum		byte "2147483647",0
 
 .code
 main PROC
@@ -81,17 +85,39 @@ main PROC
 	mDisplayString offset intro3
 	
 
-	push	offset	rePrompt
-	push	offset	promptUser
-	push	offset	BUFFER
-	push	sizeof	BUFFER
-	push	offset	byteCount
-	call	ReadVal
+	;push	offset	rePrompt
+	;push	offset	promptUser
+	;push	offset	BUFFER
+	;push	sizeof	BUFFER
+	;push	offset	byteCount
+	;call	ReadVal
+	
+		
+	mov		esi,offset testNum
+	mov		ecx,sizeof testNum
+	dec		ecx
+	mov		eax,0
+	CLD
+	conv:
+		mov		ebx,10
+		imul	ebx
+		push	eax ; save old remainder times 10
 	
 
+		xor		eax,eax
+		lodsb
+		sub		eax,48
+		mov		ebx,eax
 
+		pop		eax
+		add		eax,ebx
+		
+		;call	WriteInt
 
-	Invoke ExitProcess,0	; exit to operating system
+		LOOP conv
+		_break:
+		call	WriteInt
+		Invoke ExitProcess,0	; exit to operating system
 main ENDP
 
 ; (insert additional procedures here)
@@ -99,14 +125,24 @@ main ENDP
 ReadVal	 PROC
 	push	ebp
 	mov		ebp,esp
-		
+
 	;			+20	       +16			+12			 +8
 	;			promptUser,offset BUFFER ,sizeof BUFFER,offset byteCount
-	mGetString [ebp+20],[ebp+16],[ebp+12],[ebp+8]
+	_firstPrompt:
+		mGetString [ebp+20],[ebp+16],[ebp+12],[ebp+8]
+		JMP		_verify
+	
+	_subsequentPrompt: ; ebp+24 is the rePrompt
+		mGetString [ebp+24],[ebp+16],[ebp+12],[ebp+8]	
 
+	_verify:
 
 	mov		esi,[ebp+16] ;start of number string
-	mov		ecx,[ebp+8] ;number of bytes in string
+	mov		ecx,[ebp+8] ;number of bytes
+
+;	mov		edx,0 ; representing if there is a sign
+	mov		eax,0
+
 	verifyChars:	
 		lodsb	
 
@@ -125,28 +161,35 @@ ReadVal	 PROC
 		JMP		_continue
 		
 		_checkPlus:
-			cmp		ecx,[ebp+8]
+			cmp		ecx,[ebp+8]; check if this is the first iteration
 			JNE		_invalidChar
-		
+			;mov		edx,1
+			JMP		_continue
 		_checkNegative:
 			cmp		ecx,[ebp+8]
 			JNE		_invalidChar
+			;mov		edx,-1
+			JMP		_continue
 
 		_continue:
+		
+
 		LOOP verifyChars
-		JMP		_allValidChars ;all characters checked, nothing disallowed 
+		JMP		_allValidChars ; nothing invalid found
 
 	_invalidChar:
 		mov		edx,offset invalid
 		call	WriteString
-		JMP		_end
+		call	crlf
+		JMP		_subsequentPrompt
 	_allValidChars:
 		mov		edx,offset valid 
 		call	WriteString
-
+		call	crlf
+	
 	_end:
-	pop ebp
-	RET		16
+	pop		ebp
+	RET		20
 ReadVal	 ENDP
 
 WriteVal PROC	
